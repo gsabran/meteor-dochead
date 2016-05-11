fd5af22698422705500e7ed6b9b3360658f08cc1
Guillaume Sabran <sabranguillaume@gmail.com>
2016-05-11 12:51:12 -0700

    add some tests

--- test/server.js
+++ test/server.js
@@ -20,11 +20,12 @@ Tinytest.addAsync('Server - setTitle and getTitle', function(test, done) {
   DocHead.setTitle(id);
 });
 
-Tinytest.addAsync('Server - addMeta', function(test, done) {
+Tinytest.addAsync('Server - addMeta and setMeta', function(test, done) {
   const metaInfo = {name: "description", content: "hello content"};
   let handle = onSsrContext(function(html) {
     const metaTag = `<meta name="${metaInfo.name}" content="${metaInfo.content}" dochead="1"/>`;
     test.equal(html, metaTag);
+    test.equal(DocHead.getMeta(metaInfo.name), metaInfo);
     handle.stop();
     done();
   });
@@ -51,4 +52,46 @@ Tinytest.addAsync('Server - addLdJsonScript', function(test, done) {
     done();
   });
   DocHead.addLdJsonScript(snippet);
+});
+
+Tinytest.addAsync('Server - setDefaultTitle and getTitle', function(test, done) {
+  const defaultId = Random.id();
+  let handle = onSsrContext(function(html) {
+    const title = `<title>${defaultId}</title>`;
+    test.equal(html, title);
+    test.equal(DocHead.getTitle(), defaultId);
+    handle.stop();
+    done();
+  });
+  DocHead.setDefaultTitle(defaultId);
+  DocHead.addDefaultAttributes();
+});
+
+Tinytest.addAsync('Server - setDefaultTitle, setTitle and getTitle', function(test, done) {
+  const defaultId = Random.id();
+  const id = Random.id();
+  let handle = onSsrContext(function(html) {
+    const title = `<title>${id}</title>`;
+    test.equal(html, title);
+    test.equal(DocHead.getTitle(), id);
+    handle.stop();
+    done();
+  });
+  DocHead.setDefaultTitle(defaultId);
+  DocHead.setTitle(id);
+  DocHead.addDefaultAttributes();
+});
+
+Tinytest.addAsync('Server - setDefaultTitle and resetDefaultAttributes', function(test, done) {
+  const defaultId = Random.id();
+  let handle = onSsrContext(function(html) {
+    const title = '';
+    test.equal(html, title);
+    test.equal(DocHead.getTitle(), null);
+    handle.stop();
+    done();
+  });
+  DocHead.setDefaultTitle(defaultId);
+  DocHead.resetDefaultAttributes();
+  DocHead.addDefaultAttributes();
 });
